3d25d2f47bce90aff18b7ebfbf2f0ce9e67368b0
Iamneo <techadmin@iamneo.ai>
2023-08-08 04:01:26 +0000

    Backup Commit

--- reactapp/src/components/UI/Banner/Banner.js
+++ reactapp/src/components/UI/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import StartButton from '../Buttons/StartButton'
+import StartButton from '../Button/StartButton'
 
 const Banner = ({setIsStart, isStart}) => {
   return (
